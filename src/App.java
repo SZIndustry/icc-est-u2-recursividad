9441a0f249ba18d8b42fabcfb8c9dac38b6f2c17
SZIndustry <114554923+SZIndustry@users.noreply.github.com>
2025-04-09 17:00:04 -0500

    Ejercicios recursividad

--- src/App.java
+++ src/App.java
@@ -2,28 +2,27 @@ public class App {
     public static void main(String[] args)  {
         int n = 5;
         int resultadofinal = factorial(n);
-        System.out.println("resultado: " + resultadofinal);
+        //System.out.println("resultado: " + resultadofinal);
     }
     public static int factorial(int n) {
         if(n==0) {
-            System.out.println("Alncaze el caso base");
+           // System.out.println("Alncaze el caso base");
             return  1;
         }
         int resultado = n * factorial(n -1 );
-        System.out.println("Calculando factorial de " + n + " * factorias("+(n-1)+"-1)");
+       // System.out.println("Calculando factorial de " + n + " * factorias("+(n-1)+"-1)");
         return resultado;
     }
 
-    public int fibonacci(int n) {
-        if (n == 0) {
-            return 0;
-        }
+    EjerciciosRecursividad er = new EjerciciosRecursividad();
 
-        else if (n == 1) {
-            return 1;
-        }
-        return fibonacci(n -1) + fibonacci(n -2);
+    int resultadoFibonacci = er.fibonacci(5);
+    System.out.println("Fibonacci de " + resultadoFibonacci);
 
-    }
+    
+    int resultadoSuma = er.sumaConsecutivos(5);
+    System.out.println("Suma de los números del 1 al 5" + resultadoSuma);
+    
 
 }
+
---
+++ src/EjerciciosRecursividad.java
@@ -0,0 +1,31 @@
+public class EjerciciosRecursividad {
+    public int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n -2);
+
+    }
+
+    //public int sumaConsecutivos(int n) {
+        //if (n == 1) {
+        //    return 1;
+       // }
+       // return n + sumaConsecutivos(n - 1);
+   // }
+
+    public int sumaConsecutivos(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int resultadoParcial = sumaConsecutivos(n-1);
+        int resultado = n + resultadoParcial;
+        return resultado;
+
+
+    }
+}
+
