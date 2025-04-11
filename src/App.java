da3f8ed5359dc7d339bc77eb8835d3fc8a0f6375
SZIndustry <114554923+SZIndustry@users.noreply.github.com>
2025-04-10 19:12:44 -0500

    Clase2 recursividad

--- src/App.java
+++ src/App.java
@@ -1,30 +1,50 @@
 public class App {
     public static void main(String[] args) {
-        int n = 5;
-        int resultadofinal = factorial(n);
-        //System.out.println("resultado: " + resultadofinal);
-        
-        App app = new App();
-        app.fibonacci(n);
+
+        int base = 4;
+        int exponente = 2;
+        EjerciciosRecursividad eX = new EjerciciosRecursividad();
+        int resultado = eX.pontenciaNumero(base, exponente);
+
+        System.out.println("La potencia de " + base + " es "  + resultado);
+        System.out.println();
+
+        int numero = 123;
+        EjerciciosRecursividad eS = new EjerciciosRecursividad();
+        int resultadoSuma = eS.sumaDigitos(numero);
+        System.out.println("SUMA DE DIGITOS");
+        System.out.println(resultadoSuma);
+
+
+
+
+
+
+
+        // int n = 5;
+        // int resultadofinal = factorial(n);
+        // System.out.println("resultado: " + resultadofinal);
+
     }
     
-    public static int factorial(int n) {
-        if(n==0) {
-           // System.out.println("Alncaze el caso base");
-            return  1;
-        }
-        int resultado = n * factorial(n -1);
-       // System.out.println("Calculando factorial de " + n + " * factorias("+(n-1)+"-1)");
-        return resultado;
-    }
+    // public static int factorial(int n) {
+    //       if(n==0) {
+    //        System.out.println("Alncaze el caso base");
+    //         return  1;
+    //     }
+    //     int resultado = n * factorial(n -1);
+    //     System.out.println("Calculando factorial de " + n + " * factorias("+(n-1)+"-1)");
+    //     return resultado;
+    // }
 
-    public void fibonacci(int n) {
-        EjerciciosRecursividad er = new EjerciciosRecursividad();
+    // 
+    //     EjerciciosRecursividad er = new EjerciciosRecursividad();
 
-        int resultadoFibonacci = er.fibonacci(n);
-        System.out.println("Fibonacci de " + n + ": " + resultadoFibonacci);
+    //     int resultadoFibonacci = er.fibonacci(n);
+    //     System.out.println("Fibonacci de " + n + ": " + resultadoFibonacci);
         
-        int resultadoSuma = er.sumaConsecutivos(n);
-        System.out.println("Suma de los números del 1 al " + n + ": " + resultadoSuma);
-    }
+    //     int resultadoSuma = er.sumaConsecutivos(n);
+    //     System.out.println("Suma de los números del 1 al " + n + ": " + resultadoSuma);
+    // 
+
 }
--- src/EjerciciosRecursividad.java
+++ src/EjerciciosRecursividad.java
@@ -17,8 +17,28 @@ public class EjerciciosRecursividad {
         int resultadoParcial = sumaConsecutivos(n-1);
         int resultado = n + resultadoParcial;
         return resultado;
+    }
 
+    public int pontenciaNumero(int base, int exponente) {
+        if (exponente == 0) {
+            return 1; 
+        }
+
+        else {
+            return base * pontenciaNumero(base, exponente - 1);
+        }
 
     }
-}
 
+
+    public int sumaDigitos(int n) {
+        if (n < 10) {
+            return n;
+        }
+        else {
+            int ultimoDigito = n % 10;
+            int restoNumero = n / 10;
+            return ultimoDigito + sumaDigitos(restoNumero);
+        }
+    }
+}
