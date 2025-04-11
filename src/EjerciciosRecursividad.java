ba451dd5e4fa40c83f0478e567850d5bc3207623
sDucker <scri0962@gmail.com>
2025-04-10 22:08:25 -0600

    Tarea terminada

--- src/EjerciciosRecursividad.java
+++ src/EjerciciosRecursividad.java
@@ -54,13 +54,17 @@ public class EjerciciosRecursividad {
     }
 
     public int reverso(int n) {
+        return reversoAux(n, 0);
+    }
+    
+    public int reversoAux(int n, int invertido) {
+
         if (n == 0) {
-            return n;
-        }
-        else {
+            return invertido;
+        } else {
             int ultimoDigito = n % 10;
-            //int aux = ultimoDigito;
-            return ultimoDigito; 
+            int nuevoInvertido = invertido * 10 + ultimoDigito;
+            return reversoAux(n / 10, nuevoInvertido);
         }
     }
 }
